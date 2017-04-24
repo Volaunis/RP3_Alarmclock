a051d9286513ddb213e687b8213e386ab9bc82b4
Allan <allan@localhost>
2017-04-24 23:58:27 +0200

    Implemented control construction

---
+++ RP3_Alarmclock/Helpers/ControlHelper.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace RP3_Alarmclock.Helpers
+{
+    public static class ControlHelper
+    {
+        public static ControlInfo CreateTextBlock(int offsetLeft, int offsetTop, int width, int height, SolidColorBrush backgroundColor, SolidColorBrush foregroundColor, string text)
+        {
+            var ret = new ControlInfo();
+
+            var border = new Border();
+            border.HorizontalAlignment = HorizontalAlignment.Left;
+            border.VerticalAlignment = VerticalAlignment.Top;
+
+            border.Margin = new Thickness(offsetLeft, offsetTop, 0, 0);
+            border.Background = backgroundColor;
+
+            border.Width = width;
+            border.Height = height;
+
+            var textBlock = new TextBlock();
+            textBlock.Text = text;
+            textBlock.Width = width;
+            textBlock.Height = height;
+            textBlock.FontSize = 80;
+            textBlock.Padding = new Thickness(46, 0, 0, 0);
+            textBlock.Foreground = foregroundColor;
+
+            border.Child = textBlock;
+
+            ret.border = border;
+            ret.control = textBlock;
+
+            return ret;
+        }
+    }
+
+
+
+    public class ControlInfo
+    {
+        public Border border { get; set; }
+        public FrameworkElement control { get; set; }
+    }
+}
--- RP3_Alarmclock/MainPage.xaml.cs
+++ RP3_Alarmclock/MainPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +26,10 @@ namespace RP3_Alarmclock
     {
         private DispatcherTimer _timer;
 
+        private Grid _mainGrid;
+
+        private TextBlock tblClock;
+
         public MainPage()
         {
             this.InitializeComponent();
@@ -34,6 +39,47 @@ namespace RP3_Alarmclock
             _timer.Tick += ClockEvent;
             _timer.Interval = new TimeSpan(0, 0, 1);
             _timer.Start();
+
+            _mainGrid = new Grid();
+            _mainGrid.Background = new SolidColorBrush(Colors.Black);
+            _mainGrid.Width = 800;
+            _mainGrid.Height = 480;
+
+            Content = _mainGrid;
+
+            //var border = new Border();
+            //border.HorizontalAlignment = HorizontalAlignment.Left;
+            //border.VerticalAlignment = VerticalAlignment.Top;
+
+            //border.Margin = new Thickness(393, 10, 0, 0);
+            //border.Background = new SolidColorBrush(Colors.Red);
+
+            //border.Width = 397;
+            //border.Height = 123;
+
+
+            //_mainGrid.Children.Add(border);
+
+            //tblClock = new TextBlock();
+            //tblClock.Text = "";
+            //tblClock.Width = 397;
+            //tblClock.Height = 123;
+            //tblClock.FontSize = 80;
+            //tblClock.Padding = new Thickness(46, 0, 0, 0);
+            //tblClock.KeyDown += TblClock_KeyDown;
+
+            //border.Child = tblClock;
+
+            var textBlock = ControlHelper.CreateTextBlock(393, 10, 397, 123, new SolidColorBrush(Colors.Red), new SolidColorBrush(Colors.White), "");
+            textBlock.control.KeyDown += tblClock_KeyDown;
+            _mainGrid.Children.Add(textBlock.border);
+
+
+        }
+
+        private void TblClock_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            throw new NotImplementedException();
         }
 
         private void tblClock_KeyDown(object sender, KeyRoutedEventArgs e)
@@ -43,7 +89,7 @@ namespace RP3_Alarmclock
 
         private void ClockEvent(object sender, object e)
         {
-            tblClock.Text = DateTime.Now.ToString("HH:mm:ss");
+            ((TextBlock)sender).Text = DateTime.Now.ToString("HH:mm:ss");
         }
 
     }
