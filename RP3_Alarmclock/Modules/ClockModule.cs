11874bfff022b63a0cfdc9f7b2b7c15d3ded300d
Allan Kjærgaard <Allan Kjrgaard>
2017-04-30 00:39:06 +0200

    Fixed unsaved files derp

--- RP3_Alarmclock/Modules/ClockModule.cs
+++ RP3_Alarmclock/Modules/ClockModule.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml;
 
 namespace RP3_Alarmclock.Modules
 {
     class ClockModule : BaseModule
     {
+        private DispatcherTimer _timer;
+
+        public void InitTimer(long InitialDelay, long intervalDelay, Delegate timerEvent)
+        {
+            _timer 
+        }
+
     }
 }
